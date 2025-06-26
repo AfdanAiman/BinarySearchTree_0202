0c390edb64f5c8c3becb4523f608c9ef08903ea7
AfdanAiman <afdanaiman06@gmail.com>
2025-06-26 10:57:51 +0700

    menambahkan function preorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -100,6 +100,19 @@ public:
 
         inorder(ptr->leftchild); 
         cout << ptr->info << " "; 
-        inorder(ptr->rightchild); 
+        inorder(ptr->rightchild);
+    }
+    void preorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty." << endl;
+            return;
+        }
+        if (ptr != nullptr)
+           return;
+        cout << ptr->info << " ";
+        preorder(ptr->leftchild); 
+        preorder(ptr->rightchild);
     }
 };
