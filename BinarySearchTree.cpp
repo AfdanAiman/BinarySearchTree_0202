2be712ddf23588ccef55e00893b5bb1795533b4a
AfdanAiman <afdanaiman06@gmail.com>
2025-06-26 10:00:52 +0700

    menambahkan construktor pada class node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -4,5 +4,15 @@ using namespace std;
 class Node
 {
 public:
-   
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the Node class
+    Node()
+    {
+        leftchild = nullptr;// Initialize left child to null
+        rightchild = nullptr; // Initialize right child to null
+
+    }
 };
